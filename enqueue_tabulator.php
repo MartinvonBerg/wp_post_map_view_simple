9986b0d55a8d4efd5bd0b09e833a7805d3d3c4e0
Martin <mail@mvb1.de>
2022-02-02 09:18:02 +0100

    PHP Warning removed

--- enqueue_tabulator.php
+++ enqueue_tabulator.php
@@ -8,4 +8,4 @@ wp_enqueue_style('tabulator_css', $plugin_url . 'tabulator-master/dist/css/tabul
 
 // Load Scripts
 wp_enqueue_script('tabulator_js', $plugin_url . 'tabulator-master/dist/js/tabulator.min.js', array('jquery'), '0.5.0', true);
-wp_enqueue_script('wp_post_map_tabulator_js', $plugin_url . 'js/wp_post_map_tabulator.min.js', array('jquery'), '0.10.3', true);
+wp_enqueue_script('wp_post_map_tabulator_js', $plugin_url . 'js/wp_post_map_tabulator.min.js', array('jquery'), '0.10.4', true);
